44c7d2d81ed5d6985865511ac2c3fadef53a3589
Luke Gain <lgain64@gmail.com>
2023-10-28 14:29:27 -0500

    fix main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,10 @@
 resource "aws_lambda_function" "func" {
-    filename    = data.archive_file.zip.output_path
+    filename         = data.archive_file.zip.output_path
     source_code_hash = data.archive_file.zip.output_base64sha256
-    function_name = "func"
-    role = aws_iam_role.iam_for_lambda.arn
-    handler = "func.handler"
-    runtime = "python3.8"
+    function_name    = "func"
+    role             = aws_iam_role.iam_for_lambda.arn
+    handler          = "func.handler"
+    runtime          = "python3.8"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -27,8 +27,56 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+resource "aws_iam_policy" "iam_policy_cloud_resume" {
+  name = "aws_iam_policy_for_cloud_resume"
+  path = "/"
+  description = "AWS IAM Policy for cloud resume"
+    policy = jsonencode(
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+        {
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*",
+            "Effect": "Allow"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:UpdateItem",
+                "dynamodb:GetItem"
+            ],
+            "Resource" : "arn:aws:dynamodb:*:*:table/visitor_count_table"
+        },
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.iam_policy_cloud_resume.arn
+}
+
 data "archive_file" "zip" {
     type = "zip"
     source_dir = "${path.module}/lambda/"
     output_path = "${path.module}/packedlambda.zip"
+}
+
+resource "aws_lambda_function_url" "url1" {
+  function_name = aws_lambda_function.func.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins = ["*"]
+    allow_methods = ["*"]
+    allow_headers = ["date", "keep-alive"]
+    expose_headers = ["keep-alive", "date"]
+    max_age = 86400
+  }
 }
