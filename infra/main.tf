068cdf7bb5776f08c37afb088e266fc2d6068e38
Luke Gain <lgain64@gmail.com>
2024-04-21 20:05:04 -0500

    update python runtime

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "func" {
     function_name    = "func"
     role             = aws_iam_role.iam_for_lambda.arn
     handler          = "func.handler"
-    runtime          = "python3.8"
+    runtime          = "python3.12"
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
